993b1f85f9cbc037ba90b3d3d82697a5aa7d99f1
Julian Gabriel Maciel <jmaciel@cnrt.ar>
2022-09-27 15:36:37 -0300

    commit

--- src/AppBundle/Form/ModifProvinciaType.php
+++ src/AppBundle/Form/ModifProvinciaType.php
@@ -8,7 +8,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
